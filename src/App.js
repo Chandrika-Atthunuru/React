e254c0ee7e796060949fb8f528f027c379af5aba
Chandrika-Atthunuru <chandrikaatthunuru99@gmail.com>
2023-11-17 16:29:55 +0530

    Hey

---
+++ src/App.js
@@ -0,0 +1,410 @@
+
+
+// import Watch from './Text 1';
+// import'./Text 1.css';
+// import restarantdata from "./data"
+// import'./data.css';
+// import React,{ useState } from 'react';
+// import personsdata from "./data"
+// import React,{useEffect,useReducer,useState} from 'react';
+// import React,{useReducer, useRef, useState} from 'react';
+// import Cocktail from './Text 1'
+// import Personsdata from './Text 1'
+import React , { useState } from "react";
+
+
+
+
+
+// single compound
+// function App() {
+//   return (
+//     <div className="App">
+//      <h1 style={{color:"pink",fontSize:"40px"}}>hello</h1>
+//      <div><h1 style={{color:"purple"}}>chandrika</h1></div>
+//      <Details/>
+//      <Name/>
+//     </div>
+//   );
+// }
+
+// // multiple compounds in single app
+
+// function Name() {
+//   return (
+//     <div className="App">
+//      <Image/>
+//     </div>
+//   );
+// }
+// const Image =()=>{
+//   return(
+//     <>
+//     <h1>nature</h1>
+//     <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80" height={300} width={600}/>
+//     </>
+//   )
+// }
+// // we can import multiple components in single app
+// // function App() {
+// //     return (
+// //      <div className="App">
+// //        <Text/>
+// //       </div>
+// //     );
+// //  }
+
+// // we can add mulitiple varaibles before the return and we can call the data inside
+// function Details() {
+//   let person ={
+//     name:"chandu",
+//     place:"polisettipadu"
+//   }
+//       return (
+//        <div>
+//          <Text/>
+//          <h1>{person.name} Is coming from {person.place}</h1>
+//         </div>
+//       );
+//    }
+  //  props
+  // function App() {
+  //   return(
+  //     <div>
+  //     <Restaurant dishes="egg rice"/>
+    
+  //     </div>
+
+  //   );
+  // }
+
+  // let image="https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg"
+  // let name="srikanya"
+  // let cuisines ="cuisines: italic,indian"
+  // let rating="rating:4 star"
+  // const Restaurant=(props)=>{
+  //    console.log(props)
+  //   return(
+  //     <>
+  //     <img src={image}/>
+  //     <h3>{name}</h3>
+  //     <h3>{cuisines}</h3>
+  //     <h3>{rating}</h3>
+  //     </>
+  //   )
+  // }
+
+  // multiple varibles in probs
+  // function App() {
+  //     return(
+  //       <div>
+  //       <Restaurant image={srikanya.image} name={srikanya.name} cuisines={srikanya.cuisines} rating={srikanya.rating}  />
+  //       <Restaurant image={kfc.image} name={kfc.name} cuisines={kfc.cuisines} rating={kfc.rating}  />
+      
+  //       </div>
+  
+  //     );
+  //   }
+  //   let srikanya={
+  //   image: "https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg",
+  //    name:"srikanya",
+  //   cuisines :"cuisines: italic,indian",
+  //   rating:"rating:4 star"
+
+  //   }
+  //  let kfc={
+  //  image:"https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg",
+  //  name:"kfc",
+  //  cuisines :"cuisines: italic,indian",
+  //  rating:"5 star"
+  //  }
+  //   const Restaurant=(props)=>{
+  //      console.log(props)
+  //     return(
+  //       <>
+  //       <img src={props.image}/>
+  //       <h3>{props.name}</h3>
+  //       <h3>{props.cuisines}</h3>
+  //       <h3>{props.rating}</h3>
+  //       </>
+  //     )
+  //   }
+  // array of objects in single app
+  // function App() {
+  //   return (
+  //       <div className="two">
+  //     { 
+  //       data.map((eachobj)=>{
+  //       const {image,name,cuisines,rating}=eachobj; 
+  //     // <Restaurant image={eachobj.image} name={eachobj.name} cuisines={eachobj.cuisines} rating={eachobj.rating}/>
+  //      return <Restaurant image={image} name={name} cuisines={cuisines} rating={rating}/>
+        
+  //     })
+  //   }
+  //   </div>
+  //   )}
+  //   const Restaurant=(props)=>{
+  //          console.log(props)
+  //         return(
+  //           <>
+  //           <img src={props.image}/>
+  //           <h3>{props.name}</h3>
+  //           <h3>{props.cuisines}</h3>
+  //           <h3>{props.rating}</h3>
+  //           </>
+  //         )
+  //       }
+
+  // hooks
+  // used to create variable.in this we used two varaibles one is data and another one is used for update data
+  // function App() {
+  //   const [data,updatedata]=useState(10)
+  //   const decreasehandler=()=>{
+  //     updatedata(data-1)
+  //   }
+  //   const increasehandler=()=>{
+  //     updatedata(data+1)
+  //   }
+
+  //     return(
+  //       <div>
+  //       <button onClick={decreasehandler}>-</button>
+  //       <span>{data}</span>
+  //       <button onClick={increasehandler}>+</button>
+      
+  //       </div>
+  
+  //     );
+  //   }
+// example of usestate
+//  function App() {
+//   const[data,setdata] =useState(restarantdata)
+//   const deletehandler=(recieveid)=>{
+//     // console.log(recieveid)
+//    const filterdata=data.filter((eachobj)=>{
+//     return eachobj.id !== recieveid
+//     } )
+//     setdata(filterdata)
+//     };
+//     const updatehandler=(recieveid)=>{
+//      const updatedata=data.map((eachobj)=>{
+//       return eachobj.id === recieveid
+//      })
+//      setdata(updatedata)
+//     }
+//     return(
+//     <div>
+//     <ul>
+//       {
+//         data.map((eachobj)=>{
+//           const {image,name,cuisines,rating,id}=eachobj
+//           return (
+//           <li key={id}>
+//            <div><img src={image}/></div> 
+//            <div>{name}</div>
+//            <div>{cuisines}</div>
+//            <div>{rating}</div>
+//            <button onClick={()=> deletehandler(id)}>delete</button>
+//            <button onClick={()=> updatehandler(id)}>update</button>
+//            </li>
+//           )
+//         })
+//       }
+//     </ul>
+//     </div>
+//  )}
+
+// example
+// function App(){
+//   const[data,setdata]=useState(personsdata)
+   
+//   const deletedata=(recieveid)=>{
+//     const filterdata=data.filter((eachperson)=>{
+//     return eachperson.id!==recieveid
+//   })
+//   setdata(filterdata)
+//   }
+// return(
+//   <div>
+//    {    
+//       data.map((eachperson)=>{
+//       const {name,username,email,phone,id}=eachperson
+//       return(
+
+//       <div key={id}>
+//         <tr>   
+//        <div>{name}</div>
+//        <div>{username}</div> 
+//        <div>{email}</div>
+//        <div>{phone}</div>
+//        <button onClick={()=>deletedata(id)}>delete</button>
+//        </tr>
+//        </div>
+//        )}
+//     )}
+//   </div>
+// )
+
+// }
+// use effect
+// function App(){
+//   const url="https://jsonplaceholder.typicode.com/users"
+//   const [userdata,setdata]=useState([])
+//   const fetchuserdata=async (apiurl)=>{
+//  const response =await fetch(apiurl)
+//  const data=await response.json()
+//  setdata(data)
+// // console.log(data)
+//   }
+//   useEffect(()=>{
+//     fetchuserdata(url)
+//   },[])
+//   return(
+//     <div>
+//       {
+//       userdata.map((apiurl)=>{
+//       const{name,username,email,phone,id}=apiurl
+//       return(
+//         <li key={id}>
+//           <div>{name}</div>
+//           <div>{username}</div>
+//           <div>{email}</div>
+//           <div>{phone}</div>
+//         </li>
+//       )
+
+//     })
+//   }
+//     </div>
+//   )
+// }
+// usecontext
+// export const usercontext = React.createContext()
+// function App(){
+  
+//   return(
+//     <div>
+//       <div className='App'>
+//       <usercontext.Provider value={"coming from usecontext"}>
+//         <Text/>
+//       </usercontext.Provider>
+//       </div>
+//     </div>
+//   )
+// }
+  
+// example of use context
+//  export const usecontext = React.createContext()
+//  function App(){
+//   let product={
+//     name:"phone",
+//     price:20000,
+//     battery:"5000vh"
+//   }
+
+//   return(
+//     <>
+//     <div>
+//       <usecontext.Provider value={product.name}>
+//         <Text/>
+//       </usecontext.Provider>
+//     </div>
+    
+//     </>
+//   )
+//  }
+// usereducer
+// const intialisation={count:0}
+// function reducer(state,action){
+// switch (action.type){
+//   case "increament":return {count : state.count +1}
+//   case "decreament":return {count : state.count -1}
+//   default:throw new Error()
+// }
+// }
+
+// function App(){
+//   const [state,dispatch]=useReducer(reducer,intialisation)
+//   return(
+//     <>
+//     count:{state.count}
+//    <button onClick={()=>dispatch({type:"decreament"})}>-</button>
+//    <button onClick={()=>dispatch({type:"increament"})}>+</button>
+//     </>
+//   )
+// }
+// example for use reducer
+// function reducer(state,action){
+//   switch(action.type){
+//     case "increament_age":return{
+//     name:state.name,
+//     age:state.age+1
+//     };
+//     case "changed_name":return{
+//     name:action.nextName,
+//     age:state.age
+//     };
+//   }
+// }
+// const intialisation={name:"chandrika", age:24};
+// function App(){
+//   const [state, dispatch]=useReducer(reducer,intialisation);
+
+// const  handleButtononClick=()=>{
+//   dispatch({type:"increament_age"})
+// }
+// const  handleinputchange=(e)=>{
+//   dispatch ({
+//   type:"changed_name",
+//   nextName:e.target.value
+// })
+// }
+//  return(
+//   <div>
+//     <input value={state.name} onChange={handleinputchange}/>
+//     <button onClick={handleButtononClick}>increament age</button>
+//     <p>{state.name}.you are {state.age}</p>
+//   </div>
+//  ) 
+// }
+// useref()
+// function App(){
+//   let ref=useRef(0)
+//   function handleclick(){
+//     ref.current=ref.current+1
+//     alert("clicked"+ ref.current+"times!")
+//   }
+//   return(
+//     <div>
+//       <button onClick={handleclick}>share post</button>
+//     </div>
+//   )
+// }
+// example of useref
+function App(){
+  count [count,setcount]=useState(0)
+  count[number,setnumber]=useState(5)
+  const factorial= React.useMemo(() => fact(number),[number])
+  const handelchange=()=>{
+    setcount(count+1)
+  }
+  return(
+    <>
+    {factorial}
+    <button onClick={handelchange}>share</button>
+    {count}
+    </> 
+  )
+}
+const fact=(n)=>{
+  let answer=1;
+ for (var i=n;i>=1;i--){
+  answer=answer*1
+ }
+ console.log("Factorial called")
+ return answer
+ }
+
+
+
+export default App;
---
+++ src/Text 1.js
@@ -0,0 +1,195 @@
+// import { usercontext } from "./App";
+import React, { useEffect, useState } from "react";
+// function Text() {
+//       return (
+//         <div id="one">
+//          <h1>hello</h1>
+//          <Name/>
+//         </div>
+//       );
+//     }
+//     const Name=()=>{
+//       return(
+//         <div>
+//          <h2>chandrika atthunuru</h2>
+//         </div>
+//       )
+//     }
+// usecontext
+// const Text=()=>{
+//   return(
+//     <div>
+//       <usercontext.Consumer>
+//       {value=><div>{value}</div>}
+//       </usercontext.Consumer>
+      
+//     </div>
+//   )
+// }
+//  example of usecontext
+//  import { usecontext } from "./App";
+//  const Text=()=>{
+//   return(
+//    <>
+//    <usecontext.Consumer>
+//     {value=><div>{value}</div>}
+//    </usecontext.Consumer>
+//    </>
+//   )
+  
+//  }
+// example of useref
+// const Watch=()=>{
+//    const [starttime,setstarttime]=useState(null)
+//    const[now, setnow]=useState(null)
+//    const intervalref=useRef(null)
+//    function handlestart() {
+//       setstarttime(Date.now())
+//       setnow(Date.now())
+//       // clearInterval(intervalref.current)
+//       intervalref.current=setInterval(()=>{
+//          setstarttime(Date.now())
+//       })
+//    }
+//    function stoptime(){
+//       clearInterval(intervalref.current)
+//    }
+//    let secondsPassed=0;
+//    if(starttime !== null && now !==null){
+//       secondsPassed = (starttime-now)/1000
+//    }
+
+//    return(
+//       <>
+//       <h4>{secondsPassed.toFixed(3)}</h4>
+//       <button onClick={handlestart}>starttime</button>
+//       <button onClick={stoptime}>stoptime</button>
+//       </>
+//    )
+// }
+// const Watch=()=>{
+//    const [starttime,setstarttime]=useState(null)
+//    const [stoptime,setstoptime]=useState(null)
+//    const intervalref=useRef(null)
+//    function timestart(){
+//       setstarttime(Date.now())
+//       setstoptime(Date.now())
+//       clearInterval(intervalref.current)
+//       intervalref.current=setInterval(()=>{
+//          setstarttime(Date.now())
+//       })
+//    }
+//    function timestop(){
+//       clearInterval(intervalref.current)
+//    }
+//    let secondsPassed=0
+//    if(starttime !== null && stoptime !== null){
+//       secondsPassed=(starttime-stoptime)/1000
+//    }
+
+      
+//    return(
+//       <>
+//       <h3>{secondsPassed.toFixed(5)}</h3>
+//       <button onClick={timestart}>starttime</button>
+//       <button onClick={timestop}>stoptime</button>
+//       </>
+//    )
+// }
+
+
+
+//     export default Watch ;
+// real time example for usestate,useeffect
+// const url="https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+// const Cocktail =()=>{
+//    const [userdata,setuserdata]=useState([])
+//    const[searchterm,setsearchterm]=useState('')
+//    const[loading,setloading]=useState(false);
+//    const[iserror,setiserror]=useState({status:false,msg:''})
+// const fetchdata=async (apiurl)=>{
+//    setloading(true)
+//    setiserror({status:false,msg:''})
+//    try{
+//    const response=await fetch(apiurl);
+//     const {drinks} = await response.json();
+//     setuserdata(drinks)
+//     setloading(false)
+//     setiserror({status:false,msg:''})
+//     if(!drinks){
+//       throw new Error("Data not found")
+//     }
+// }
+// catch(error){
+//    setloading(false)
+//    setiserror({status:true,msg:error.message ||"something went wrong..."})
+// }
+// }
+// useEffect(()=>{
+//    const correcturl=`${url}${searchterm}`
+//    fetchdata(correcturl)
+// },[searchterm])
+
+//    return(
+//       < div className="one">
+//       <div>
+//          <input type="text" className="search" placeholder="search" value={searchterm} onChange={(e)=>setsearchterm(e.target.value)}/>
+//       </div>
+//       <hr id="line"/>
+//       {loading && !iserror?.status&&<h5>loading.....</h5>}
+//       {iserror?.status && <h5 style={{color:"red"}}>{iserror.msg}</h5>}
+//       {
+//        !loading && !iserror?.status &&
+//        <ul id="direction">
+//          {
+//          userdata.map((eachDrink)=>{
+//             const {idDrink,strDrink,strDrinkThumb}=eachDrink;
+//             return(
+//                <li key={idDrink} type="none" >
+//                <div className="image">
+//                <img src={strDrinkThumb} />
+//                </div>
+//                <div className="brandname">{strDrink}</div>
+//                </li>
+//             )
+//          })
+
+//       }
+//       </ul>
+//       }
+//       </div>
+//    )
+// }
+// export default Cocktail;
+// example 
+// const url="https://jsonplaceholder.typicode.com/posts"
+// const Personsdata=()=>{
+//    const[usersdata,setdata]=useState([])
+
+//    const fetchdata=async(apiurl)=>{
+//       const response=await fetch(apiurl)
+//       const data=await response.json()
+//       setdata(data)
+//    }
+//    useEffect(()=>{
+//       fetchdata(url)
+//    })
+//    return(
+//       <>
+//       {
+//       usersdata.map((eachobj)=>{
+//          const{userid,id,title,body}=eachobj
+//          return(
+//             <li key={id}>
+//             <div>{userid}</div>
+//             <div>{title}</div>
+//             <div>{body}</div>
+//             </li>
+//          )
+
+//       })
+//       }
+//       </>
+//    )
+// }
+// export default Personsdata;
